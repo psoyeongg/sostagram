f5279edb490487fc01db579fa96a19d6d4955727
parksoyeong <psoyeongg@gmail.com>
2018-12-06 01:12:43 +0900

    Deleting my own comment

--- sostagram/images/urls.py
+++ sostagram/images/urls.py
@@ -4,16 +4,8 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
-    path("<int:image_id>/comment/", view=views.CommentOnImage.as_view(), name="comment_image")
+    path("<int:image_id>/likes/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/comments/", view=views.CommentOnImage.as_view(), name="comment_image"),
+    path("comments/<int:comment_id>/", view=views.Comment.as_view(), name="comment")
 ]
 
-
-
-
-# /images/3/like/
-
-# 0. create the url and the view (done)
-# 1. take the id from the url (done)
-# 2. we want to find an image with the id
-# 3. we want to create a like for that image
