b79a0f15f00c7c680ed5e7b5c2cb6be528e37fe1
parksoyeong <psoyeongg@gmail.com>
2018-12-06 00:23:10 +0900

    Restricting Likes

--- sostagram/images/urls.py
+++ sostagram/images/urls.py
@@ -4,4 +4,15 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
+    path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image")
 ]
+
+
+
+
+# /images/3/like/
+
+# 0. create the url and the view (done)
+# 1. take the id from the url (done)
+# 2. we want to find an image with the id
+# 3. we want to create a like for that image
