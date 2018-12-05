c015a0e775af04cdff050d395c15ef9a19a30b18
parksoyeong <psoyeongg@gmail.com>
2018-12-06 00:57:33 +0900

    Commenting on an Image

--- sostagram/images/urls.py
+++ sostagram/images/urls.py
@@ -4,7 +4,8 @@ from . import views
 app_name = "images"
 urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
-    path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image")
+    path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
+    path("<int:image_id>/comment/", view=views.CommentOnImage.as_view(), name="comment_image")
 ]
 
 
