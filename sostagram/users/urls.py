766c1c902695e14955789d1bebf601d09109d5f0
parksoyeong <psoyeongg@gmail.com>
2018-12-07 23:42:50 +0900

    Getting Following List

--- sostagram/users/urls.py
+++ sostagram/users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("<int:user_id>/unfollow/", view=views.UnFollowUser.as_view(), name="unfollow_users"),
     path("<username>/", view=views.UserProfile.as_view(), name="user_profile"),
     path("<username>/followers", view=views.UserFollowers.as_view(), name="user_followers"),
+    path("<username>/following", view=views.UserFollowing.as_view(), name="user_following"),
 
 ]
