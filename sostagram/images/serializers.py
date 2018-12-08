c791ff62a27b5741a99b435157f1fa5e0f970e5c
parksoyeong <psoyeongg@gmail.com>
2018-12-08 22:49:30 +0900

    Getting Single Photo

--- sostagram/images/serializers.py
+++ sostagram/images/serializers.py
@@ -69,5 +69,6 @@ class ImageSerializer(serializers.ModelSerializer):
             'caption',
             'comments',
             'like_count',
-            'creator'
+            'creator',
+            'created_at',
         )
