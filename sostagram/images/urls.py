d559e8e575bcdc52bebb58e16586a35e27eddadb
parksoyeong <psoyeongg@gmail.com>
2018-12-08 22:37:07 +0900

    Moderating comments on my image

--- sostagram/images/urls.py
+++ sostagram/images/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("", view=views.Feed.as_view(), name="feed"),
     path("<int:image_id>/like/", view=views.LikeImage.as_view(), name="like_image"),
     path("<int:image_id>/unlike/", view=views.UnLikeImage.as_view(), name="unlike_image"),
+    path("<int:image_id>/comments/<int:comment_id>/", view=views.ModerateComments.as_view(), name="comment_image"),
     path("<int:image_id>/comments/", view=views.CommentOnImage.as_view(), name="comment_image"),
     path("comments/<int:comment_id>/", view=views.Comment.as_view(), name="comment"),
     path("search/", view=views.Search.as_view(), name="search"),
--- sostagram/images/views.py
+++ sostagram/images/views.py
@@ -152,3 +152,19 @@ class  Search(APIView):
 
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
+
+
+class  ModerateComments(APIView):
+
+    def delete(self, request, image_id, comment_id, format=None):
+
+        user = request.user
+
+        try:
+            comment_to_delete = models.Comment.objects.get(
+                id=comment_id, image__id=image_id, image__creator=user)
+            comment_to_delete.delete()
+        except models.Comment.DoseNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
