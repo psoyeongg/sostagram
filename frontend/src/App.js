d19c808f0533d85224d9d763adc1f6b75e33d963
parksoyeong <psoyeongg@gmail.com>
2018-12-09 18:54:02 +0900

    Redux Middlewares-Logger

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,9 @@
 import React, { Component } from 'react';
-import styles from './App.module.scss';
+import styles from 'App.module.scss';
 
 class App extends Component {
   render() {
-    return (
-      <div className={styles.app}>
-        <header className={styles.appHeader}>
-          <p>
-            Edit <code>src/App.js</code>안녕하세요^^
-          </p>
-          <a
-            className={styles.appLink}
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <div className={styles.App} />
   }
 }
 
