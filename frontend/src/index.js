b4183c9f1b1b72caa984ac79722d9b5f59f1e801
parksoyeong <psoyeongg@gmail.com>
2018-12-10 02:17:52 +0900

    etc..

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,11 +4,17 @@ import { Provider } from 'react-redux';
 import store from 'redux/configureStore';
 import 'index.css';
 import App from 'App';
+import I18n from 'redux-i18n';
+import { tranlations } from "translations";
+
+// import "ReactotronConfig";
 
 ReactDOM.render(
-        <Provider store={store}>
+    <Provider store={store}>
+        <I18n translations={tranlations} initialLang="en" fallbackLang="en">
             <App />
-        </Provider>,
+        </I18n>
+    </Provider>,
     document.getElementById('root')
 );
 
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,6 +1,9 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { i18nState } from 'redux-i18n';
 import users from 'redux/modules/users';
+// import Reactotron from 'ReactotronConfig';
 
 const env = process.env.NODE_ENV;
 
@@ -11,12 +14,21 @@ if (env === 'development') {
     middlewares.push(logger);
 }
 
-
 const reducer = combineReducers({
-    users
-})
+    users,
+    i18nState,
+});
 
-let store = initialState => 
-    createStore(reducer, applyMiddleware(...middlewares));
+let store;
+
+if (env === 'development') {
+    store = initialState => 
+    createStore(reducer, composeWithDevTools(applyMiddleware(...middlewares))
+    );
+} else {
+    store = initialState => 
+        createStore(reducer, applyMiddleware(...middlewares));
+}
+
 
 export default store();
