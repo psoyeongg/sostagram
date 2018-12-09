a4618c3ae34539cb570cb263c8706d31621ee816
parksoyeong <psoyeongg@gmail.com>
2018-12-09 17:42:06 +0900

    Serving Webpack Bundles with Django

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 from rest_framework_jwt.views import obtain_jwt_token
+from sostagram import views
 
 urlpatterns = [
     # Django Admin, use {% url 'admin:index' %}
@@ -25,7 +26,8 @@ urlpatterns = [
         include("sostagram.notifications.urls", namespace="notifications"),
     ),
     path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
+    path("", views.ReactAppView.as_view()),
+    
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
