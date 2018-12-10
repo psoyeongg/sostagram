d6a089126dc3bf25e452c9a4dff55bf3d34519fa
parksoyeong <psoyeongg@gmail.com>
2018-12-10 14:33:40 +0900

    Edit frontend folder

---
+++ frontend/src/components/App/presenter.js
@@ -0,0 +1,37 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Route, Switch } from "react-router-dom";
+import './styles.module.scss';
+import Auth from "../Auth";
+import Footer from "../Footer";
+import Navigation from "../Navigation";
+import Feed from "../Feed";
+import Explore from "../Explore";
+import Search from "../Search";
+
+const App = props => [
+  props.isLoggedIn ? <Navigation key={1} /> : null,
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  <Footer key={3} />
+];
+
+App.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired
+};
+
+const PrivateRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Feed} />
+    <Route path="/explore" component={Explore} />
+    <Route path="/search/:searchTerm" component={Search} />
+  </Switch>
+);
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path="/" component={Auth} />
+    <Route path="/recover" render={() => "recover password"} />
+  </Switch>
+);
+
+export default App;
